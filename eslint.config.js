9c35bf3cbb1643dc2114b8a4e077b1fde6d43ec6
DelCampoRepository <delcampoapps@gmail.com>
2026-07-14 15:03:39 -0700

    chote:configure ESLint and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
 
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
 
     extends: [
       js.configs.recommended,
@@ -23,17 +23,17 @@ export default defineConfig([
     },
 
     rules: {
-      "no-console": "warn",
+      '@typescript-eslint/no-explicit-any': 'error',
 
-      "@typescript-eslint/no-explicit-any": "error",
-
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
         {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
         },
       ],
+
+      'no-console': 'warn',
     },
   },
-]);
+])
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-
 function App() {
   return (
     <>
-     <div className="min-h-screen bg-green-300 flex items-center justify-center">
-      <h1 className="text-red-500 bg-yellow-300 p-8">
-  hola
-</h1>
-    </div>
+      <div className='min-h-screen bg-green-300 flex items-center justify-center'>
+        <h1 className='text-red-500 bg-yellow-300 p-8'>hola</h1>
+      </div>
     </>
   )
 }
